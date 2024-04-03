7e9132dfda56b8a7d56c8abf1c0a0f4721737f47
Vinícius Henriques <viniciuspinholi6@gmail.com>
2024-04-03 19:31:53 -0300

    Implementados serviços de negócios

---
+++ src/Business/Services/FornecedorService.cs
@@ -0,0 +1,39 @@
+﻿using Business.Interfaces;
+using Business.Models;
+
+namespace Business.Services
+{
+	internal class FornecedorService : BaseService, IFornecedorService
+	{
+		private readonly IFornecedorRepository _fornecedorRepository;
+
+		public FornecedorService(IFornecedorRepository forncedorRepository)
+		{
+			_fornecedorRepository = forncedorRepository;
+		}
+
+		public async Task Adicionar(Fornecedor fornecedor)
+		{
+			//validar se a entidade é consistente...
+
+			//validar se ja nao existe outro fornecedor com o mesmo doc
+
+			await _fornecedorRepository.Adicionar(fornecedor);
+		}
+
+		public async Task Atualizar(Fornecedor fornecedor)
+		{
+			await _fornecedorRepository.Atualizar(fornecedor);
+		}
+
+		public async Task Remover(Guid id)
+		{
+			await _fornecedorRepository.Remover(id);
+		}
+
+		public void Dispose()
+		{
+			_fornecedorRepository?.Dispose();//Dê o dispose na instancia do Fornecedor Repository. Quando o garbage collector passar, vai tirar esse cara da memória
+		}
+	}
+}
---
+++ src/Business/Services/ProdutoService.cs
@@ -0,0 +1,39 @@
+﻿using Business.Interfaces;
+using Business.Models;
+
+namespace Business.Services
+{
+	internal class ProdutoService : BaseService, IProdutoService
+	{
+		private readonly IProdutoRepository _produtoRepository;
+
+		public ProdutoService(IProdutoRepository produtoRepository)
+		{
+			_produtoRepository = produtoRepository;
+		}
+
+		public async Task Adicionar(Produto produto)
+		{
+			//validar se a entidade é consistente...
+
+			//validar se ja nao existe outro fornecedor com o mesmo doc
+
+			await _produtoRepository.Adicionar(produto);
+		}
+
+		public async Task Atualizar(Produto produto)
+		{
+			await _produtoRepository.Atualizar(produto);
+		}
+
+		public async Task Remover(Guid id)
+		{
+			await _produtoRepository.Remover(id);
+		}
+
+		public void Dispose()
+		{
+			_produtoRepository?.Dispose();//Dê o dispose na instancia do Fornecedor Repository. Quando o garbage collector passar, vai tirar esse cara da memória
+		}
+	}
+}
